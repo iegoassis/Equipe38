a91da196cde5695c0321c3a9238030144aebde81
Kalebe Ribeiro <105246557+KalebStonkss@users.noreply.github.com>
2025-02-27 15:50:22 -0300

    Update Template.swift

--- Pomodoro/Template.swift
+++ Pomodoro/Template.swift
@@ -12,18 +12,21 @@ struct Template: View{
     var cicloDiario: [PomodoroPoint]
     var body: some View {
         
+        var body: some View {
+        
         VStack {
             Text("Estatísticas")
-                .font(.largeTitle)
+                .font(.pomodoroUI(.satoshiHeading1))
                 .padding()
-            
+            Text("Diária")
+                .font(.pomodoroUI(.satoshiHeading3))
+                .frame(maxWidth: .infinity,alignment: .leading)
             HStack(spacing:-16) {
                 VStack {
                     ZStack{
                         Rectangle()
-                          .foregroundColor(.clear)
-                          .frame(width: 150, height: 80)
-                          .background(Color(red: 0.41, green: 0.64, blue: 0.93).opacity(0.13))
+                          .foregroundColor(.blue)
+                          .frame(width: 175, height: 88)
                           .cornerRadius(25)
                           .overlay(
                             RoundedRectangle(cornerRadius: 25)
@@ -31,12 +34,14 @@ struct Template: View{
                               .stroke(Color(red: 0.41, green: 0.64, blue: 0.93), lineWidth: 3)
                           )
                                                 
-                        VStack{
-                            Text("\(ciclos)")
-                                .font(.title)
-                                .foregroundColor(.blue)
+                        VStack(spacing:10){
                             Text("Ciclos completos")
-                                .font(.subheadline)
+                                .font(.pomodoroUI(.satoshiBody2Medium))
+                                .foregroundColor(.white)
+                            Text("\(ciclos)")
+                                .font(.pomodoroUI(.satoshiBody1Medium))
+                                .foregroundColor(.white)
+                            
                         }
                     }
                 }
@@ -44,26 +49,48 @@ struct Template: View{
                 VStack {
                     ZStack{
                         Rectangle()
-                          .foregroundColor(.clear)
-                          .frame(width: 150, height: 80)
-                          .background(Color(red: 0.41, green: 0.64, blue: 0.93).opacity(0.13))
+                          .foregroundColor(.blue)
+                          .frame(width: 175, height: 88)
                           .cornerRadius(25)
-                          .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                              .inset(by: 1.5)
-                              .stroke(Color(red: 0.41, green: 0.64, blue: 0.93), lineWidth: 3)
-                          )
-                        VStack{
-                            Text("25 mins")
-                                .font(.title)
-                                .foregroundColor(.red)
+                        VStack(spacing:10){
+                            
                             Text("Duração dos ciclos")
-                                .font(.subheadline)
+                                .font(.pomodoroUI(.satoshiBody2Medium))
+                                .foregroundColor(.white)
+                            Text("25 mins")
+                                .font(.pomodoroUI(.satoshiBody1Medium))
+                                .foregroundColor(.white)
                         }
                     }
                 }
                 .padding()
             }
+            
+            Spacer()
+            Chart(cicloDiario){ point in
+                BarMark(x: .value("Dia", formatDate(point.day)), y: .value("Ciclos",point.ciclos))
+                
+            }
+            .chartYAxis {
+                AxisMarks(position: .leading) // Isso move os rótulos para a esquerda
+            }
+            .frame(height:300)
+            Text("Gráfico de desempenho")
+                .fontWeight(.bold)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(8)
+                .foregroundColor(Color.white)
+        }
+        .padding()
+    }
+    private func formatDate(_ inputDate : Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM"
+        return formatter.string(from: inputDate)
+    }
+}
+
             
             Spacer()
             Chart(cicloDiario){ point in
